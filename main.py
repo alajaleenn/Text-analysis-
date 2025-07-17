5126d911da0542d289e25db646eb2b71c4152130
Najla Alajaleen <alajaleenn1@gmail.com>
2025-07-17 06:48:09 +0300

    supporting docx

--- main.py
+++ main.py
@@ -1,43 +1,55 @@
 from collections import Counter
 import string
+from docx import Document
 
-print("add your book path:")
-path=input()
 
+print("what is your file type? .txt or .docx or .pdf?")
+filetype=input().strip()
+
+if filetype == ".docx" or filetype == ".txt":
+  print("add your book path:")
+  path=input().strip()
 
 #Reading the text book
-with open(path,"r",encoding="utf-8") as book:
-    content=book.read()
+  if filetype==".txt":
+    with open(path,"r",encoding="utf-8") as book:
+      content=book.read()
+  elif filetype==".docx":
+    doc=Document(path)
+    paragraphs=doc.paragraphs
+    content="\n".join([p.text for p in paragraphs])
+
 
 
 #Calculating number of sentences 
-sentences= content.split(".")
-sentences=[s.strip() for s in sentences if s.strip()]
-numOfSentences=len(sentences)
+  sentences= content.split(".")
+  sentences=[s.strip() for s in sentences if s.strip()]
+  numOfSentences=len(sentences)
 
 #Cleaning the data
-words=content.split(" ")
-words=[word.strip(string.punctuation).lower() for word in words if word.strip(string.punctuation)]
+  words=content.split()
+  words=[word.strip(string.punctuation).lower() for word in words if word.strip(string.punctuation)]
 
 
 #Calculating number of words 
-numOFwords=len(words)
+  numOFwords=len(words)
 
 
 
 #average number of words in a sentence 
-average=round(numOFwords/numOfSentences)
+  average=round(numOFwords/numOfSentences)
 
 #Searching for the unique words
-uniqueWords=Counter(words)
-topWords=uniqueWords.most_common(5)
+  uniqueWords=Counter(words)
+  topWords=uniqueWords.most_common(5)
 
 
-print("--------Text Analysis Summary--------")
-print("Number of words in the book: ",numOFwords)
-print("Average number of words per sentence: ",average)
-print("Number of uniqe words: ",len(uniqueWords))
-print("Top 5 words repeted: ")
-for word, count in topWords:
+  print("--------Text Analysis Summary--------")
+  print("Number of words in the book: ",numOFwords)
+  print("Average number of words per sentence: ",average)
+  print("Number of uniqe words: ",len(uniqueWords))
+  print("Top 5 words repeted: ")
+  for word, count in topWords:
    print(f" ({word}) is repeted {count} times")
-
+else:
+    print("Unsupported file type")
